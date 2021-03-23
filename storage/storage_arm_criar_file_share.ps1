c3e4bf294547f77645436ca355eced03b84f3e61
Philipe Cella <philipepi@gmail.com>
2021-03-23 16:31:54 -0300

    Script para criar fileshare com arm e powershell

---
+++ storage/storage_arm_criar_file_share.ps1
@@ -0,0 +1,9 @@
+$DisplayRGNames = (Get-AzResourceGroup).ResourceGroupName
+Write-Host -ForegroundColor Blue "List of RGroups:"
+$DisplayRGNames
+$resourceGroupName = Read-Host -Prompt "Enter the Resource Group name" 
+$DisplaySGNames = Get-AzStorageAccount -ResourceGroupName $resourceGroupName
+Write-Host -ForegroundColor yellow "List of StorageAccounts:" $DisplaySGNames.StorageAccountName 
+
+ 
+New-AzResourceGroupDeployment -ResourceGroupName $resourceGroupName -TemplateUri "C:\Users\phili\Desktop\lab\storage\templates\storage_arm_criar_file_share.json"
